1108c0b6d98bc555bf410b419ed3a568ee2f63f0
Ankit Patel <yugap1806@gmail.com>
2025-01-10 14:44:45 +0530

    Genrate table  _p18

---
+++ level1_cf/GenerateTable.java
@@ -0,0 +1,20 @@
+//Program to print table using for loop
+import java.util.Scanner;
+
+class GenerateTable{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking as input
+        System.out.print("Enter a number to generate its multiplication table - ");
+        int number = sc.nextInt();
+
+        // Generating multiplication table of given number
+ 
+        for (int currentMultiplier = 1; currentMultiplier <= 10; currentMultiplier++) {
+            System.out.println(number + " x " + currentMultiplier + " = " + (number * currentMultiplier));
+        }
+
+        sc.close();
+    }
+}
