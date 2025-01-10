597e781d6d86678707e91e3cdc7fa41523b98e0f
Ankit Patel <yugap1806@gmail.com>
2025-01-10 14:29:00 +0530

    divisible by five_p1

---
+++ level1_cf/DivisibleFive.java
@@ -0,0 +1,22 @@
+//Program to Find number is divisible by 5 or not
+import java.util.Scanner;
+
+class DivisibleFive {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+        System.out.print("Enter a number - ");
+		int inputNumber = sc.nextInt();
+		boolean isDivisibleByFive;
+		
+        // Checking if the number is divisible by 5
+        if (inputNumber % 5 == 0) {
+            isDivisibleByFive = true;
+        } else {
+            isDivisibleByFive = false;
+        }
+		
+		System.out.println("Is the number " + inputNumber + " divisible by 5? " + isDivisibleByFive);
+		sc.close();
+    }
+}
