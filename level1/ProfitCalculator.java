4f372d5da54420627e281fe1c542212298a285f8
Ankit Patel <yugap1806@gmail.com>
2025-01-10 09:56:11 +0530

    Profit and cost price_p4

---
+++ level1/ProfitCalculator.java
@@ -0,0 +1,19 @@
+// Program to calculate profit 
+
+public class ProfitCalculator {
+
+    public static void main(String[] args) {
+
+        double costPrice = 129;
+        double sellingPrice = 191;
+
+        // Calculate profit and profit percentage
+        double profit = sellingPrice - costPrice;
+        double profitPercentage = (profit / costPrice) * 100;
+
+        // Output the result
+        System.out.println("The Cost Price is INR " + costPrice + " and Selling Price is INR " + sellingPrice +"\nThe Profit is INR " + profit + " and the Profit Percentage is " + profitPercentage + "%");
+
+
+    }
+}
