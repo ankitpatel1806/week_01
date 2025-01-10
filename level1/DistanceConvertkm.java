3e5e3d6b8c682adf8b7d3dd34e7c04576b9ad7bd
Ankit Patel <yugap1806@gmail.com>
2025-01-10 09:55:06 +0530

    km to miles_p3

---
+++ level1/DistanceConvertkm.java
@@ -0,0 +1,11 @@
+// Program to convert Km to miles
+public class DistanceConvertKm{
+	public static void main(String[]args){
+	//constant
+	double distanceInKm =10.8;
+	//conversion of kilo in miles
+	double distanceInMiles = distanceInKm * 1.6;
+	//output
+	System.out.println("The distance " + distanceInKm + " km in miles is " + distanceInMiles);
+}
+}
