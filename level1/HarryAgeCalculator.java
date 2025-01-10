8470ac983dfdd0fdd0ed245ce6e2a59747e12c32
Ankit Patel <yugap1806@gmail.com>
2025-01-10 09:54:03 +0530

    Harry's age_p1

---
+++ level1/HarryAgeCalculator.java
@@ -0,0 +1,15 @@
+// Program to calculate Age of Harry
+
+public class HarryAgeCalculator {
+    public static void main(String[] args) {
+        // Constants
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        // Calculate age
+        int age = currentYear - birthYear;
+
+        // Output
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
